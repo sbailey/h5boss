45790e53661d0ee9f42e589a176e50a771f8fc0e
Jialin Liu <valiantljk@gmail.com>
2016-08-29 22:11:36 -0700

    update catalog parsing and read

--- h5boss_c/parse_node.h
+++ h5boss_c/parse_node.h
@@ -5,13 +5,13 @@ struct Fiber{
     int count;     // number of counts, e.g., 188000
     char ** keys;  // a list of fiber dataset name, e.g., 5732/56326/936/coadd
     char ** values;// a list of file path, /global/cscratch1/sd/jialin/h5boss/5732-56326.hdf5
-}; 
+};
 struct Catalog{
     int count;       // number of catalog rows, e.g., 19800
     char ** plate_mjd;// a list of plate/mjd pair, e.g., 3690/55182
-    char ** fiber_id; // a list of fiber id, e.g., 181 
+    char ** fiber_loffset; // a list of fiber original offset, e.g., 0
     char ** filepath; // a list of file path, e.g., global/cscratch1/sd/jialin/h5boss/3690-55182.hdf5
-    char ** fiber_offset; // a list of fiber offsets, e.g., 0
+    char ** fiber_goffset; // a list of fiber new offsets, e.g., 1
 };
 //split a string by delimiter, return a char **
 char ** str_split(char* a_str, const char a_delim);
