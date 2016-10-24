dfb4b276027342315434a73e3f2f6604117ee44b
valiantljk <valiantljk@gmail.com>
2016-10-24 11:36:02 -0700

    fix sql

--- h5boss_py/h5boss/select_add.py
+++ h5boss_py/h5boss/select_add.py
@@ -23,5 +23,6 @@ def select_add(infiles, infile, pmflist):
     plates=[i.split(' ')[0] for i in to_add]
     mjds=[i.split(' ')[0] for i in to_add]
     fibers=[i.split(' ')[0] for i in to_add]
+    print ("len of plates:%d"%(len(plates)))
     print ("plates:",plates)
     select(infiles,infile,plates,mjds,fibers)
